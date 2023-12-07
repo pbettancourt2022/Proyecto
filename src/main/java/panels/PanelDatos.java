4705eca2d26042a76cc8bc7e0e462a9f91196370
pbettancourt2022 <pbettancourt2022@udec.cl>
2023-12-07 15:23:56 -0300

    Avance en el panel de datos
    Ahora hay un boton para confirmar datos, y los guarda

--- src/main/java/panels/PanelDatos.java
+++ src/main/java/panels/PanelDatos.java
@@ -2,10 +2,14 @@ package panels;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class PanelDatos extends JPanel {
+    private JTextField campoNombre;
+    private JRadioButton siRadioButton;
+    private JRadioButton noRadioButton;
+
     public PanelDatos() {
         super();
         setBounds(920, 40, 380, 530);
@@ -17,25 +21,25 @@ public class PanelDatos extends JPanel {
         this.add(titulo);
 
         JLabel etiquetaNombre = new JLabel("Nombre");
-        etiquetaNombre.setBounds(25, 75, 60, 20);
+        etiquetaNombre.setBounds(25, 125, 60, 20);
         this.add(etiquetaNombre);
 
-        JTextField campoNombre = new JTextField();
-        campoNombre.setBounds(25, 102, 330, 20);
+        campoNombre = new JTextField();
+        campoNombre.setBounds(25, 152, 330, 20);
         this.add(campoNombre);
 
         JLabel tne = new JLabel("Pasaje estudiante", SwingConstants.CENTER);
-        tne.setBounds(25, 152, 110, 120);
+        tne.setBounds(25, 160, 110, 120);
         tne.setFont(new Font("Arial", Font.BOLD, 12));
         this.add(tne);
 
         // Crear los JRadioButton "Sí" y "No"
-        JRadioButton siRadioButton = new JRadioButton("Sí");
-        JRadioButton noRadioButton = new JRadioButton("No");
+        siRadioButton = new JRadioButton("Sí");
+        noRadioButton = new JRadioButton("No");
 
         // Establecer los límites y añadir los botones al panel
-        siRadioButton.setBounds(140, 170, 50, 20);
-        noRadioButton.setBounds(190, 170, 50, 20);
+        siRadioButton.setBounds(140, 210, 50, 20);
+        noRadioButton.setBounds(190, 210, 50, 20);
         this.add(siRadioButton);
         this.add(noRadioButton);
 
@@ -43,6 +47,24 @@ public class PanelDatos extends JPanel {
         ButtonGroup buttonGroup = new ButtonGroup();
         buttonGroup.add(siRadioButton);
         buttonGroup.add(noRadioButton);
+
+        // Botón "Confirmar Datos"
+        JButton confirmarDatosButton = new JButton("Confirmar Datos");
+        confirmarDatosButton.setBounds(25, 260, 150, 30);
+        confirmarDatosButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Obtener el texto del campo de nombre y la opción seleccionada
+                String nombre = campoNombre.getText();
+                String seleccionPasaje = (siRadioButton.isSelected()) ? "Sí" : "No";
+
+                // Aquí puedes hacer lo que necesites con los datos recopilados
+                // Por ejemplo, imprimirlos en la consola
+                System.out.println("Nombre: " + nombre);
+                System.out.println("Pasaje estudiante: " + seleccionPasaje);
+            }
+        });
+        this.add(confirmarDatosButton);
     }
 
     public void handleClick(int x, int y) {
